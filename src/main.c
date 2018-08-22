a623601c79167d534536f59df553cf90e2a33bd7
Kwezi Mayikana <https://github.com/kmayika>
2018-08-22 23:14:23 +0200

    step 1

--- src/main.c
+++ src/main.c
@@ -10,17 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "../../../../minilibx/mlx.h"
+#include <math.h>
+#include <stdlib.h>
+#define mapWidth 24
+#define mapHeight 24
+
+int worldMap[mapWidth][mapHeight]=
+{
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
 
 int		main(/*int ac, char **av*/)
 {
 	double pos_x = 22, pos_y = 12;//x and y start positions
 	double dir_x = - 1, dir_y = 0;// initial direction vector
-	double plane_x = 0, plan_y = 0.66;//camera plane
-	double time = 0;//time of current frame
-	double old_time = 0; //time of old frame
+	double plane_x = 0, plane_y = 0.66;//camera plane
+//	double time = 0;//time of current frame
+//	double old_time = 0; //time of old frame
 	int	x = 0;
-	int	w;
 
 	void *mlx;
 	void *win;
@@ -29,14 +60,91 @@ int		main(/*int ac, char **av*/)
 	win = mlx_new_window(mlx, 500,500, "Wolf3D");
 	while (1)
 	{
-		while(x < w)
+		while(x < mapWidth)
 		{
-			double camera_x = 2 * x / double(w) - 1;
-			double ray_dir_x = dir_x + plane_x * camera_x;
-			double ray_dir_y = dir_y + plan_y * camera_x;
- 
-}
+			double camera_x = 2 * x / mapWidth - 1;
+			double rayDirX = dir_x + plane_x * camera_x;
+			double rayDirY = dir_y + plane_y * camera_x;
+			//which box of the map we're in
+      		int mapX = (int)pos_x;
+      		int mapY = (int)pos_y;
 
+      	//length of ray from current position to next x or y-side
+      		double sideDistX;
+      		double sideDistY;
+
+       	//length of ray from one x or y-side to next x or y-side
+      		double deltaDistX = fabs(1 / rayDirX);
+     		double deltaDistY = fabs(1 / rayDirY);
+      		double perpWallDist;
+
+      	//what direction to step in x or y-direction (either +1 or -1)
+      		int stepX;
+      		int stepY;
+
+      		int hit = 0; //was there a wall hit?
+      		int side; //was a NS or a EW wall hit?
+			//calculate step and initial sideDist
+			if (rayDirX < 0)
+     		{
+        	stepX = -1;
+        	sideDistX = (pos_x - mapX) * deltaDistX;
+      		}
+      		else
+      		{
+				stepX = 1;
+        		sideDistX = (mapX + 1.0 - pos_x) * deltaDistX;
+      		}
+      		if (rayDirY < 0)
+     		{
+        		stepY = -1;
+        		sideDistY = (pos_y - mapY) * deltaDistY;
+      		}
+      		else
+      		{
+        		stepY = 1;
+        		sideDistY = (mapY + 1.0 - pos_y) * deltaDistY;
+      		}
+			//perform DDA
+      		while (hit == 0)
+      		{
+        //jump to next map square, OR in x-direction, OR in y-direction
+        		if (sideDistX < sideDistY)
+        		{
+          			sideDistX += deltaDistX;
+          			mapX += stepX;
+          			side = 0;
+       			}
+        		else
+        		{
+          			sideDistY += deltaDistY;
+          			mapY += stepY;
+          			side = 1;
+        		}
+        //Check if ray has hit a wall
+        		if (worldMap[mapX][mapY] > 0)
+					hit = 1;
+      		}
+			//Calculate distance projected on camera direction (Euclidean distance will give fisheye effect!)
+      		if (side == 0)
+			  perpWallDist = (mapX - pos_x + (1 - stepX) / 2) / rayDirX;
+      		else
+			  perpWallDist = (mapY - pos_y + (1 - stepY) / 2) / rayDirY;
+			//Calculate height of line to draw on screen
+      		int lineHeight = (int)(mapHeight / perpWallDist);
+
+      //calculate lowest and highest pixel to fill in current stripe
+      		int drawStart = -lineHeight / 2 + mapHeight / 2;
+      		if(drawStart < 0)
+			  	drawStart = 0;
+      		int drawEnd = lineHeight / 2 + mapHeight / 2;
+      		if(drawEnd >= mapHeight)
+	  			drawEnd = mapHeight - 1;
+			mlx_pixel_put(mlx,win,drawStart,drawEnd, 0xFFFFFF);
+
+			x++;
+
+		}
 	}
 	mlx_loop(mlx);
 }
